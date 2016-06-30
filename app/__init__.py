3122365588d2723e471557739a9d8605f021c0a7
Hieu Ha-Trung <hieuht@vnoss.org>
2016-06-30 10:22:02 +0700

    fix PEP8

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template
+from app.mod_wifi.controllers import mod_wifi as wifi_module
+from app.mod_dashboard.controllers import mod_dashboard as dashboard_module
+
+
 app = Flask(__name__)
 app.config.from_pyfile("../config.cfg")
 
@@ -7,8 +11,6 @@ app.config.from_pyfile("../config.cfg")
 def not_found(error):
     return render_template('404.html'), 404
 
-from app.mod_wifi.controllers import mod_wifi as wifi_module
-from app.mod_dashboard.controllers import mod_dashboard as dashboard_module
 
 app.register_blueprint(wifi_module)
 app.register_blueprint(dashboard_module)
