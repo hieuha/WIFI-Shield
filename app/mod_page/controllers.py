34126c6e5ef7658af6cc99936226d83239cc7c2a
Hieu Ha-Trung <hieuht@vnoss.org>
2016-06-30 23:58:42 +0700

    update menu

--- app/mod_page/controllers.py
+++ app/mod_page/controllers.py
@@ -6,3 +6,8 @@ mod_page = Blueprint('page', __name__, url_prefix='/')
 @mod_page.route("/", methods=["GET"])
 def index():
     return render_template("home/index.html")
+
+
+@mod_page.route("/how-it-works", methods=["GET"])
+def how():
+    return render_template("home/how.html")
