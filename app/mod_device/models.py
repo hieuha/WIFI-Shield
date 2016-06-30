d7a2fcb30025132fbae0167afe36148d7c10e043
Hieu Ha-Trung <hieuht@vnoss.org>
2016-06-30 18:24:49 +0700

    Device module

--- app/mod_device/models.py
+++ app/mod_device/models.py
@@ -4,13 +4,13 @@ import netifaces
 
 class Device(object):
     def __init__(self):
-        pass
+        self.list_interfaces = dict()
 
-
-    @staticmethod
-    def interfaces():
-        lst_interfaces = dict()
+    def interfaces(self):
         for interface in netifaces.interfaces():
             for link in netifaces.ifaddresses(interface)[netifaces.AF_INET]:
-                lst_interfaces[interface] = link['addr']
-        return lst_interfaces
+                self.list_interfaces[interface] = link['addr']
+        return self.list_interfaces
+
+    def battery(self):
+        pass
