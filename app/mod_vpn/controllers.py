2f4817a1d875477e1988f9fd1639363250d72687
Hieu Ha-Trung <hieuht@vnoss.org>
2016-07-01 15:48:56 +0700

    add: list vpn

--- app/mod_vpn/controllers.py
+++ app/mod_vpn/controllers.py
@@ -1,3 +1,12 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, jsonify
+from app.mod_vpn.models import VPN
 mod_vpn = Blueprint('vpn', __name__, url_prefix='/vpn')
+vpn = VPN()
+
+
+@mod_vpn.route("/list", methods=["GET"])
+def list_vpn():
+    response = {"list": list()}
+    vpns = vpn.get_list()
+    response["list"] = vpns
+    return jsonify(response)
--- app/mod_vpn/models.py
+++ app/mod_vpn/models.py
@@ -1,7 +1,29 @@
+import os
+from app import app
+from re import search
+
+VPN_FOLDER = app.config["BASE_DIR"] + "/openvpn/"
+
+
+def parse_ip(conf):
+    if os.path.exists(conf):
+        f = open(conf, "r")
+        content = f.read()
+        try:
+            re_ip = search(r'remote(.+?)1194', content)
+            ip = str(re_ip.group(1)).strip()
+        except Exception, e:
+            print e
+            ip = None
+    return ip
+
+
+
 class VPN(object):
 
     def __init__(self):
-        pass
+        self.vpns = dict()
+
 
     def connect(self):
         pass
@@ -12,5 +34,14 @@ class VPN(object):
     def status(self):
         pass
 
-    def list(self):
-        pass
+    def get_list(self):
+        vpn_configs = os.listdir(VPN_FOLDER)
+        _vpn = {"conf": None,
+               "ip": None,
+               "status": None}
+        for vpn in vpn_configs:
+            conf = VPN_FOLDER + vpn + "/" + "client.ovpn"
+            _vpn["conf"] = conf
+            _vpn["ip"] = parse_ip(conf)
+            self.vpns[vpn] = _vpn
+        return self.vpns
